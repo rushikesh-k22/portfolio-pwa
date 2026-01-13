e61ac258d9dd9e750c385d04eb28d1ae83f8028c
rushikesh-k22 <rushikeshkadam22072006@gmail.com>
2026-01-13 01:07:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function setTheme(theme) {
+    document.body.className = theme === 'light' ? '' : theme;
+}
+
+/* Contact form */
 document.getElementById("contactForm").addEventListener("submit", function(e) {
     e.preventDefault();
     document.getElementById("message").innerText =
